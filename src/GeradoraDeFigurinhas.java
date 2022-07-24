f0256c3be2ecee52f998bab29a8864e5404f6a2d
Marcelo Dias <34683289+Marcelo-Dias@users.noreply.github.com>
2022-07-24 08:37:20 -0300

    2ª aula

---
+++ src/GeradoraDeFigurinhas.java
@@ -0,0 +1,50 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+//import java.io.FileInputStream;
+import java.io.InputStream;
+//import java.net.URL;
+
+import javax.imageio.ImageIO;
+
+public class GeradoraDeFigurinhas {
+    
+    public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
+
+        // leitura da imagem
+        //BufferedImage imagemOriginal = ImageIO.read(new File("entrada/filme.jpg"));
+        //InputStream inputStream = new FileInputStream(new File("entrada/filme.jpg"));
+        //InputStream inputStream = new URL("https://imersao-java-apis.s3.amazonaws.com/TopMovies_1.jpg").openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // criar nova imagem em memória com transparência e com tamanho novo
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        // copiar a imagem original para nova imagem (em memória)
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        // configurar a fonte
+        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
+        graphics.setColor(Color.YELLOW);
+        graphics.setFont(fonte);
+
+        // escrever uma frase na nova imagem
+        graphics.drawString("TOPZERA", 100, novaAltura - 100);
+
+        // escrever a nova imagem em um arquivo
+        //ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
+
+    }
+
+    //public static void main(String[] args) throws Exception {
+    //    var geradora = new GeradoraDeFigurinhas();
+    //    geradora.cria();
+    //}
+}
